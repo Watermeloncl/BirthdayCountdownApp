d896db3b3af37fadb96a0f9fcc90ae24876ba7fb
Watermeloncl <devinfrandsen@gmail.com>
2018-07-12 11:58:12 -0600

    ending july twelfth

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 class Picker extends Component {
     
-    constructor (props) {
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
           startDate: moment()
         };
